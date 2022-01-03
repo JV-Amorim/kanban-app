6f3cde46825c178cbd152437f88861b8153e60db
JV-Amorim <joaovitoramorimx@gmail.com>
2022-01-03 20:14:31 -0300

    feat: svg-icon module and component

---
+++ src/app/shared/svg-icon/svg-icon.component.spec.ts
@@ -0,0 +1,27 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SvgIconComponent } from './svg-icon.component';
+
+describe('SvgIconComponent', () => {
+
+  let component: SvgIconComponent;
+  let fixture: ComponentFixture<SvgIconComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SvgIconComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SvgIconComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+});
