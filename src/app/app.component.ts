329aa7f6794f79160474c30acc753c55f59da613
JV-Amorim <joaovitoramorimx@gmail.com>
2022-01-03 19:04:36 -0300

    feat: remove Angular default project content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'kanban-app';
-}
+export class AppComponent { }
